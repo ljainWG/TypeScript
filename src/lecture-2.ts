c0724159ef5c019df178f4ad69f37a03900db849
Lakshay Jain <Lakshay.Jain@watchguard.com>
2024-12-31 14:11:32 +0530

    arrays

--- src/lecture-2.ts
+++ src/lecture-2.ts
@@ -23,3 +23,27 @@ function render_2(document: any, age : number){
     console.log(document);
     console.log(age);
 }
+
+
+let numbers_js = [1,2,3,'4'];   // js array can have any datatype in it any they can be different as well
+// this above numbers_js variable has datatype of number | string
+// let numbers_ts_1: number [] = [1,2,3,'4']; // to restrict this above one we do like this
+let numbers_ts_2: number [] = [1,2,3,4];
+// we can remove explicitly number if all elements are number
+let numbers_ts_3 = [1,2,3,4];
+
+let anyValue = [];  // datatype is any
+anyValue[0]=1;
+anyValue[2]="typescript";
+
+// tuples
+// fixed length array where each element has a particular type, used when working with pair of values
+let user: [number, string] = [1,"typescript"];
+// let user_2: [number, string] = [1,"typescript",0];  
+// let user_3: [number, string] = [1];
+
+// let account: [number, string] = [];  //error
+let account: [number, string] = [1,"typescript"];
+account.push(1);            // we can push new elements
+// console.log(account);   
+// this is some gap or bugs in ts, which will be removed in future
